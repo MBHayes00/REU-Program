df59d7ff0fb9ad5f18b9cb137c39f2c71dd17dd2
MBHayes00 <mbh9947@rit.edu>
2021-06-02 14:37:11 -0400

    Cube moves

---
+++ REUProgram/Assets/REU/Scripts/TestScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float speed;
+    public Vector3 myVec;
+    public Quaternion myRot;
+    public GameObject myFriend;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0.5f, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation *= Quaternion.Euler(-10, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation *= Quaternion.Euler(10, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.rotation *= Quaternion.Inverse(transform.rotation);
+        }
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            if(gameObject.name == "OriginalCube")
+                transform.position = myFriend.transform.position + new Vector3(2, 0, 0);
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
